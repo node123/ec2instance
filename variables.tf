b1e05fd86c5c97ef63d17b83db55cc5affcbc3ed
Devops <7wc.info@gmail.com>
2018-09-20 16:12:19 +0530

    created ec2 instance script using terraform

---
+++ variables.tf
@@ -0,0 +1,13 @@
+#AWS Config
+
+variable "aws_access_key" {
+  default = "AKIATQAVOB6BPMMSW37U"
+}
+
+variable "aws_secret_key" {
+  default = "0b6m6uc0IHi0FwuxSfWV99DGSk3voS399qijjYZM"
+}
+
+variable "aws_region" {
+  default = "us-east-1"
+}
