0df63f22886a46b3bb174c9ee10fb7b03c8568b9
Bhaskar K <kbhaskar@idexcel.internal>
2018-09-27 17:54:05 +0530

    Updated code

---
+++ jenkinsfile.sh
@@ -0,0 +1,45 @@
+#! /bin/bash
+
+pipeline {
+    agent {
+        node {
+            label 'master'
+        }
+    }
+
+    stages {
+
+        stage('terraform started') {
+            steps {
+                sh 'echo "Started...!" '
+            }
+        }
+        stage('git clone') {
+            steps {
+                sh 'sudo rm -r *;sudo git clone https://git-codecommit.us-east-1.amazonaws.com/v1/repos/terraformec2/'
+            }
+        }
+        stage('tfsvars create'){
+            steps {
+                sh 'sudo cp /home/ubuntu/vars.tf ./jenkins/'
+            }
+        }
+        stage('terraform init') {
+            steps {
+                sh 'sudo /home/ubuntu/terraform init ./jenkins'
+            }
+        }
+        stage('terraform plan') {
+            steps {
+                sh 'ls ./jenkins; sudo /home/ubuntu/terraform plan ./jenkins'
+            }
+        }
+        stage('terraform ended') {
+            steps {
+                sh 'echo "Ended....!!"'
+            }
+        }
+
+        
+    }
+}
--- main.tf
+++ main.tf
@@ -1,8 +1,34 @@
- resource "aws_instance" "my-test-instance" {
-  ami             = "${data.aws_ami.ubuntu.id}"
-  instance_type   = "t2.micro"
+provider "aws" {
+<<<<<<< HEAD
+  region = "eu-east-1"
+=======
+  region = "us-east-1"
+>>>>>>> 39756cf70b580e7acf7b68c7aebbc0568dc8a75e
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-20180912"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["366486834717"]
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-0f6f33f5e191c9e21"
+  instance_type = "t2.micro"
 
   tags {
-    Name = "test-instance"
+    Name = "HelloWorld"
   }
- }
+}
